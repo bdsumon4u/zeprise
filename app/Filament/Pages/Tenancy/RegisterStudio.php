2385bdb272ab338fc79bdfb9a5b85f7205090fd1
Sumon Ahmed <bdsumon4u@gmail.com>
2024-01-01 09:21:54 +0600

    wip

--- app/Filament/Pages/Tenancy/RegisterStudio.php
+++ app/Filament/Pages/Tenancy/RegisterStudio.php
@@ -4,45 +4,119 @@ namespace App\Filament\Pages\Tenancy;
 
 use App\Models\Studio;
 use BezhanSalleh\FilamentShield\Support\Utils;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
 use Filament\Facades\Filament;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
 use Filament\Forms\Form;
 use Filament\Forms\Get;
 use Filament\Forms\Set;
-use Filament\Pages\Tenancy\RegisterTenant;
+use Filament\Pages\Concerns;
+use Filament\Pages\Concerns\InteractsWithFormActions;
+use Filament\Pages\SimplePage;
+use Filament\Panel;
+use Filament\Support\Exceptions\Halt;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use LucasDotVin\Soulbscription\Models\Plan;
 
-class RegisterStudio extends RegisterTenant
+use function Filament\authorize;
+
+/**
+ * @property Form $form
+ */
+class RegisterStudio extends SimplePage
 {
+    use Concerns\HasRoutes;
+    use InteractsWithFormActions;
+
+    protected ?string $maxWidth = '7xl';
+
+    /**
+     * @var view-string
+     */
+    protected static string $view = 'filament-panels::pages.tenancy.register-tenant';
+
+    /**
+     * @var array<string, mixed> | null
+     */
+    public ?array $data = [];
+
+    public ?Model $tenant = null;
+
     public static function getLabel(): string
     {
         return 'Register studio';
     }
 
-    public static function canView(): bool
+    public static function routes(Panel $panel): void
     {
-        if (! Filament::auth()->check()) {
-            return true;
-        }
+        $slug = static::getSlug();
 
-        return Filament::auth()->user()->studios->count() < 3;
+        Route::get("/{$slug}", static::class)
+            ->middleware(static::getRouteMiddleware($panel))
+            ->withoutMiddleware(static::getWithoutRouteMiddleware($panel))
+            ->name('registration');
     }
 
-    public function form(Form $form): Form
+    /**
+     * @return string | array<string>
+     */
+    public static function getRouteMiddleware(Panel $panel): string | array
     {
-        return $form
-            ->schema([
-                TextInput::make('name')
-                    ->afterStateUpdated(function (Get $get, Set $set, ?string $state) {
-                        $set('slug', Str::slug($state));
-                    })
-                    ->live(onBlur: true)
-                    ->required(),
-                TextInput::make('slug')
-                    ->required(),
-            ]);
+        return [
+            ...(static::isEmailVerificationRequired($panel) ? [static::getEmailVerifiedMiddleware($panel)] : []),
+            ...Arr::wrap(static::$routeMiddleware),
+        ];
+    }
+
+    public function mount(): void
+    {
+        abort_unless(static::canView(), 404);
+
+        $this->form->fill();
+    }
+
+    /**
+     * @param  array<string, mixed>  $data
+     * @return array<string, mixed>
+     */
+    protected function mutateFormDataBeforeRegister(array $data): array
+    {
+        return $data;
+    }
+
+    public function register(): void
+    {
+        try {
+            $this->callHook('beforeValidate');
+
+            $data = $this->form->getState();
+
+            $this->callHook('afterValidate');
+
+            $data = $this->mutateFormDataBeforeRegister($data);
+
+            $this->callHook('beforeRegister');
+
+            $this->tenant = $this->handleRegistration($data);
+
+            $this->form->model($this->tenant)->saveRelationships();
+
+            $this->callHook('afterRegister');
+        } catch (Halt $exception) {
+            return;
+        }
+
+        if ($redirectUrl = $this->getRedirectUrl()) {
+            $this->redirect($redirectUrl);
+        }
     }
 
     protected function handleRegistration(array $data): Studio
@@ -69,5 +143,106 @@ class RegisterStudio extends RegisterTenant
 
             return $business;
         });
+    }
+
+    protected function getRedirectUrl(): ?string
+    {
+        return Filament::getUrl($this->tenant);
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form->schema([
+            Wizard::make([
+                Wizard\Step::make('Studio Information')->schema([
+                    TextInput::make('name')
+                        ->afterStateUpdated(function (Get $get, Set $set, ?string $state) {
+                            $set('slug', Str::slug($state));
+                        })
+                        ->live(onBlur: true)
+                        ->required(),
+                    TextInput::make('slug')
+                        ->required(),
+                ]),
+                Wizard\Step::make('Address Information')->schema([
+                    
+                ]),
+                Wizard\Step::make('Social Information')->schema([
+                    
+                ]),
+            ]),
+        ]);
+    }
+
+    /**
+     * @return array<int | string, string | Form>
+     */
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->model($this->getModel())
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    public function getModel(): string
+    {
+        return Filament::getTenantModel();
+    }
+
+    public function getTitle(): string | Htmlable
+    {
+        return static::getLabel();
+    }
+
+    public static function getSlug(): string
+    {
+        return static::$slug ?? 'new';
+    }
+
+    public function hasLogo(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<Action | ActionGroup>
+     */
+    protected function getFormActions(): array
+    {
+        return [
+            // $this->getRegisterFormAction(),
+        ];
+    }
+
+    public function getRegisterFormAction(): Action
+    {
+        return Action::make('register')
+            ->label(static::getLabel())
+            ->submit('register');
+    }
+
+    protected function hasFullWidthFormActions(): bool
+    {
+        return true;
+    }
+
+    public static function canView(): bool
+    {
+
+        if (!Filament::auth()->check()) {
+            return true;
+        }
+
+        return Filament::auth()->user()->studios->count() < 3;
+
+        try {
+            return authorize('create', Filament::getTenantModel())->allowed();
+        } catch (AuthorizationException $exception) {
+            return $exception->toResponse()->allowed();
+        }
     }
 }
